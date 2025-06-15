54951b7612e8171ed00fea54f75143dfc234f65c
Andy Waine <amwtablet@gmail.com>
2025-06-15 13:57:15 -0400

    XCode Init new app

---
+++ AuthDemo/AuthDemo/AuthDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthDemoApp.swift
+//  AuthDemo
+//
+//  Created by Andy Waine on 6/15/25.
+//
+
+import SwiftUI
+
+@main
+struct AuthDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthDemo/AuthDemoTests/AuthDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  AuthDemoTests.swift
+//  AuthDemoTests
+//
+//  Created by Andy Waine on 6/15/25.
+//
+
+import Testing
+
+struct AuthDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
